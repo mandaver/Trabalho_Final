78082a7c31da13a7aec5eca0c404096821bba275
Victor Roberto <90845665+mandaver@users.noreply.github.com>
2023-06-26 18:34:50 -0400

    ObjectNotFoundException.java

--- user-api/src/main/java/br/com/valdir/userapi/services/exceptions/ObjectNotFoundException.java
+++ user-api/src/main/java/br/com/valdir/userapi/services/exceptions/ObjectNotFoundException.java
@@ -1,4 +1,4 @@
-package br.com.valdir.userapi.services.exceptions;
+package br.com.victor.userapi.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException{ // Construtor que recebe uma mensagem de erro e repassa para a superclasse RuntimeException
 
