05b9e068a92ea01b61e17b804dc0df9bb5f19da0
Victor Roberto <90845665+mandaver@users.noreply.github.com>
2023-06-26 19:07:58 -0400

    EurekaServer

--- eureka-server/src/test/java/br/com/valdir/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/br/com/valdir/eurekaserver/EurekaServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EurekaServerApplicationTests {
 
     @Test
-    void contextLoads() {  // Método de teste para verificar se o contexto da aplicação é carregado corretamente
+    void contextLoads() {  // Método de teste para verificar se o contexto da aplicação é carregado corretamente.
     }
 
 }
